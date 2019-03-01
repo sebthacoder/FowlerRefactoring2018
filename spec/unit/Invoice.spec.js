8fb9252cb6d997ee0690242eb8bdd03486ccd565
Seb <BWXO768@S4CJ3091.bwb.de>
2019-03-01 16:54:27 +0100

    totalVolumeCredits()

--- spec/unit/Invoice.spec.js
+++ spec/unit/Invoice.spec.js
@@ -8,11 +8,12 @@ describe("create invoice", function() {
 
   var plays = require('./plays.json');
   var invoices = require('./invoices.json')
+  var invoice = invoices[0]
 
  
   it("should print invoice", function() {
     var expectedString = `Statement for BigCo Hamlet: US$ 650.00 (55 seats) As You Like It: US$ 580.00 (35 seats) Othello: US$ 500.00 (40 seats)Amount owed is US$ 1,730.00You earned 47 credits`.replace(/\s/gm,"")
-    var actual = statementFile.statement(invoices[0], plays).replace(/\s/gm,"")
+    var actual = statementFile.statement(invoice, plays).replace(/\s/gm,"")
     console.log(actual)
     console.log(expectedString)
     expect(actual).toEqual(expectedString);
