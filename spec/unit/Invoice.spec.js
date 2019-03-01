ff0be0f95f40579358b0fa375f1cfc7747e39f56
Seb <BWXO768@S4CJ3091.bwb.de>
2019-03-01 13:28:25 +0100

    Test toEqual

--- spec/unit/Invoice.spec.js
+++ spec/unit/Invoice.spec.js
@@ -11,10 +11,26 @@ describe("create invoice", function() {
 
  
   it("should print invoice", function() {
-    var expectedString = `Statement for BigCo Hamlet: US$ 650.00 (55 seats)  As You Like It: US$ 580.00 (35 seats)  Othello: US$ 500.00 (40 seats) Amount owed is US$ 1,730.00 You earned 47 credits`.replace(/\n/gm,"").normalize
-    var actual = statementFile.statement(invoices[0], plays).replace(/\n/gm,"").normalize
+    var expectedString = `Statement for BigCo Hamlet: US$ 650.00 (55 seats) As You Like It: US$ 580.00 (35 seats) Othello: US$ 500.00 (40 seats)Amount owed is US$ 1,730.00You earned 47 credits`.replace(/\s/gm,"")
+    var actual = statementFile.statement(invoices[0], plays).replace(/\s/gm,"")
+    console.log(actual)
+    console.log(expectedString)
     expect(actual).toEqual(expectedString);
 
   });
 
+/*
+  it("should eqaul Strings", function() {
+    var expectedString = `Statement for BigCo\n`
+    + ` Hamlet: US$ 650.00 (55 seats)\n`
+    +` As You Like It: US$ 580.00 (35 seats)\n`
+    +` Othello: US$ 500.00 (40 seats)\n`
+    +`Amount owed is US$ 1,730.00\n`
+    + `You earned 47 credits\n`
+    var actual = statementFile.statement(invoices[0], plays)
+    console.log(actual)
+    console.log(expectedString)
+    expect(actual).toContain(expectedString);
+  });
+  */
 });
