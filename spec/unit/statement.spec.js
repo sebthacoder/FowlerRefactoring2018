2170e05184225f61b479b415d252cc9b224ef5d8
Seb <BWXO768@S4CJ3091.bwb.de>
2019-03-06 11:42:02 +0100

    second import coorection

--- spec/unit/statement.spec.js
+++ spec/unit/statement.spec.js
@@ -1,15 +1,10 @@
 import {statement} from '../../src/statementFile'
-// C:\Dev\RefaktorJS\ch11\lib\statementFile.js
-//const statementFile = require('../../src/statementFile');
-
+import plays from './plays.json'
+import invoices from './invoices.json'
 
 
 describe("create invoice", function() {
-
-  var plays = require('./plays.json');
-  var invoices = require('./invoices.json')
   var invoice = invoices[0]
-
  
   it("should print invoice", function() {
     var expectedString = `Statement for BigCo Hamlet: US$ 650.00 (55 seats) As You Like It: US$ 580.00 (35 seats) Othello: US$ 500.00 (40 seats)Amount owed is US$ 1,730.00You earned 47 credits`.replace(/\s/gm,"")
@@ -20,18 +15,4 @@ describe("create invoice", function() {
 
   });
 
-/*
-  it("should eqaul Strings", function() {
-    var expectedString = `Statement for BigCo\n`
-    + ` Hamlet: US$ 650.00 (55 seats)\n`
-    +` As You Like It: US$ 580.00 (35 seats)\n`
-    +` Othello: US$ 500.00 (40 seats)\n`
-    +`Amount owed is US$ 1,730.00\n`
-    + `You earned 47 credits\n`
-    var actual = statementFile.statement(invoices[0], plays)
-    console.log(actual)
-    console.log(expectedString)
-    expect(actual).toContain(expectedString);
-  });
-  */
 });
