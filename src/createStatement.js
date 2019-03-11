d47a18bb56541d5dd0fd28664141eaa25c5f512a
Seb <BWXO768@S4CJ3091.bwb.de>
2019-03-11 15:47:51 +0100

    moved amout to Comedy

--- src/createStatement.js
+++ src/createStatement.js
@@ -10,12 +10,7 @@ class PerformanceCalculator {
             case "tragedy":
                 throw 'Fehler: nicht überschrieben'
             case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
+                throw "Mist"
             default:
                 throw new Error(`unknown type: ${this.play.type}`);
         }
@@ -41,7 +36,16 @@ class TragedyCalculator extends PerformanceCalculator {
     }
 }
 
-class ComedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {
+    get amount(){
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+}
 
 function createPerformanceCalculator(aPerformance, aPlay) {
     switch(aPlay.type) {
